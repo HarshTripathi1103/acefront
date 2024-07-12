09153f3ab1d0d22c8c84a7c2c3e7ac2568b02788
Harry <mynameisharsh143@gmail.com>
2024-07-13 01:13:48 +0530

    nestedNavigation

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,28 +1,3 @@
-import { Stack } from "expo-router";
-import {Tabs} from "expo-router";
+import {Tabs} from 'expo-router';
 
-export default function Rootlayout(){
-  return(
-    <>
-    <Stack
-      screenOptions={{
-        headerStyle:{
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle:{
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen name="index" options={{}}/>
-      <Stack.Screen name = "details"/>
-    </Stack>
-
-    {/* <Tabs>
-    <Tabs.Screen name="index" />
-    <Tabs.Screen name="details" />
-
-    </Tabs> */}
-    </>
-  )
-}
+export default Tabs;
